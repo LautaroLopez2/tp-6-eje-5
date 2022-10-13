8d16807a4aa8ed30d241bdad465d31f1fa421b18
LautaroLopez2 <lautalopez2448@gmail.com>
2022-10-12 21:34:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace tp_6_eje_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string entrada;
+            int num, num1;
+            num = 0;
+            num1 = 0;
+
+            Console.WriteLine("Escriba un numero entero");
+            entrada = Console.ReadLine();
+            num = Convert.ToInt32(entrada);
+
+            Console.WriteLine("Escriba un numero entero");
+            entrada = Console.ReadLine();
+            num1 = Convert.ToInt32(entrada);
+
+            if (num == num1)
+            {
+                Console.WriteLine("Son iguales");
+            }
+            else
+            {
+                if (num < num1)
+                {
+                    Console.WriteLine("Es mayor es {0}", num1);
+                }
+                else
+                {
+                    Console.WriteLine("Es menor es {0} " + num1);
+                }
+            }
+
+        }
+    }
+}
